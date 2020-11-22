b9cf7185750d97d4d863e1fc8bf1b095b5723c54
Lana Botchorishvili <lanakona.lb@gmail.com>
2020-11-22 14:57:15 +0200

    added browse to main

--- homework3/src/components/Browse.vue
+++ homework3/src/components/Browse.vue
@@ -5,8 +5,7 @@
       <div class="profile" v-for="(profile,index) in profiles" :key="index">
         <img alt="profile picture"  class = "profile-picture" :src="profile.avatar">
         <h3>{{ profile.firstname }} {{ profile.lastname }}</h3>
-        <h1>me</h1>
-        <button name="follow" class = "follow-button">Follow</button>
+        <button name="follow" class = "follow-button" @click="followProfile">Follow</button>
       </div>
     </section>
   </div>
@@ -19,80 +18,29 @@ export default {
   name: 'Browse',
   components: {Navbar},
   data() {
-        return {
-            profiles: [],
-        };
+        return{
+          follow: false
+        }
     },
-    async created() {
-        const profilesResponse = await fetch('https://private-anon-2b0cfa86a8-wad20postit.apiary-mock.com/profiles');
-        this.profiles = await profilesResponse.json();
+  methods: {
+    followProfile(){
+      
+    }
+  },
+
+  computed: {
+    profiles() {
+      return this.$store.state.profiles;
     },
+  },
+  mounted() {
+    this.$store.dispatch('getProfiles');
+  },
 };
 </script>
 
 <style scoped>
-header {
-  position: fixed;
-  width: 100%;
-  top: 0;
-  z-index: 1;
-}
 
-header:hover {
-  box-shadow: 0 -20px 30px #4d4d4d;
-}
-
-nav {
-  display: flex;
-  background-color: #ffffff;
-  align-items: center;
-}
-
-nav div {
-  height: 30px;
-  flex-grow: 4;
-  padding: 10px;
-}
-
-nav div img {
-  height: 100%;
-  width: 30px;
-  margin-left: 15px;
-  border-radius: 100%;
-  object-fit: cover;
-  object-position: top center;
-}
-
-nav div.search-container > input {
-  box-sizing: border-box;
-  height: 30px;
-  width: 80%;
-  margin: 0;
-  padding: 5px;
-  border: 1px solid #e0e0e0;
-}
-
-nav div.search-container > button {
-  height: 30px;
-  width: 20%;
-  margin: 0;
-  padding: 5px;
-  border-top-left-radius: 0;
-  border-bottom-left-radius: 0;
-  color: #ffffff;
-  background-color: #01579b;
-  border: none;
-  border-top-right-radius: 4px;
-  border-bottom-right-radius: 4px;
-}
-nav div.search-container > button:hover {
-  box-shadow: 0 0 5px rgba(38, 50, 56, 0.7);
-  cursor: pointer;
-}
-
-.drop-down-container span {
-  display: block;
-}
 
 .profile-container {
     width: 50%;
@@ -120,5 +68,20 @@ nav div.search-container > button:hover {
     object-fit: cover;
     object-position: top;
     margin: 5px;
+}
+
+.follow-button {
+  padding: 8px 16px;
+  margin: 4px 0;
+  color: #ffffff;
+  background-color: #75048b;
+  border: none;
+  border-radius: 4px;
+}
+
+.follow-button.followed {
+  color: #75048b;
+  background-color: #ffffff;
+  border: 1px solid #75048b;
 }
 </style>
--- homework3/src/main.js
+++ homework3/src/main.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router';
 import Vuex from 'vuex';
 import Login from '@/components/Login';
 import Index from '@/components/Index';
+import Browse from "@/components/Browse";
 import store from './store';
 
 Vue.config.productionTip = false;
@@ -13,6 +14,7 @@ Vue.use(Vuex);
 const routes = [
     { path: '/', component: Login },
     { path: '/index', component: Index },
+    { path: '/browse', component: Browse },
 ];
 
 const router = new VueRouter({ routes });
